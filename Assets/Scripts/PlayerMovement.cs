eea93b02c31978056051c1e3c49bcadf6eaf8932
JordanRaia <jordanraiapi@gmail.com>
2024-09-14 06:58:36 -0500

    Added moving right animation and removed flip in movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,6 @@ public class PlayerMovement : MonoBehaviour
     public Animator animator;
 
     Vector2 movement;
-    private bool isFacingRight = false; // Tracks whether the player is facing right
 
     // Update is called once per frame
     void Update()
@@ -22,28 +21,11 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.magnitude);
-
-        if (movement.x > 0 && !isFacingRight)
-        {
-            Flip();
-        }
-        else if (movement.x < 0 && isFacingRight)
-        {
-            Flip();
-        }
     }
 
     void FixedUpdate()
     {
         // Movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-    }
-
-    void Flip()
-    {
-        isFacingRight = !isFacingRight;
-        Vector3 scale = transform.localScale;
-        scale.x *= -1;
-        transform.localScale = scale;
     }
 }
